64fa30e3e5cad48ac4d0230ec68c211fd140d45b
Vitaly Plivachuk <vitaly.plivachuk@gmail.com>
2018-09-05 20:09:05 +0300

    Initial Commit

---
+++ GBKSOFT-Layout-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GBKSOFT-Layout-Test
+//
+//  Created by Vitaly Plivachuk on 9/5/18.
+//  Copyright © 2018 Vitaly Plivachuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
